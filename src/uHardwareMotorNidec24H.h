dc92a95d1da037b4b64ad899ea625c8264108f00
Sebastian Kopf <seb.kopf@gmail.com>
2025-12-24 13:02:42 -0700

    implement fine-adjustment based on speed measurement

--- src/uHardwareMotorNidec24H.h
+++ src/uHardwareMotorNidec24H.h
@@ -2,6 +2,7 @@
 
 #include "uTypedef.h"
 #include "Particle.h"
+#include "uCoreEnums.h"
 
 // stirrer motor
 class ThardwareMotor : public TmenuHandle
@@ -29,11 +30,7 @@ private:
         decoderISR.signal();
     };
 
-    // rpm limits (inclusive)
-    const dtypes::uint16 FminRpm = 50;
-    const dtypes::uint16 FmaxRpm = 5000;
-
-    // resulting step limits (will be updated later)
+    // step limits (will be refined based on the max and min RPM sdds variables later)
     dtypes::uint16 FminStep = 0;
     dtypes::uint16 FmaxStep = 4095;
 
@@ -44,26 +41,28 @@ private:
         dtypes::uint16 stepMax; // exclusive (except last)
         dtypes::float32 rpmMin; // inclusive
         dtypes::float32 rpmMax; // inclusive for last
-        dtypes::float32 b;      // intercept_mean  (RPM at step=0 for this calibration)
-        dtypes::float32 m;      // slope_mean      (RPM per step)
+        dtypes::float32 b;      // intercept_mean  (steps at RPM=0 for this calibration)
+        dtypes::float32 m;      // slope_mean      (steps per RPM)
     };
 
     // calibration data
     static constexpr Calibration calibrations[4] = {
         // stepMin stepMax   rpmMin   rpmMax    b(intercept)   m(slope)
         {0, 1000, 236.0f, 1352.0f, 111.0f, 0.693f},
-        {1000, 2000, 1335.0f, 2846.0f, 118.0f, 0.688f},
+        {1000, 2000, 1335.0f, 2846.0f, 118.0f, 0.693f},
         {2000, 3000, 2812.0f, 4307.0f, 75.9f, 0.702f},
         {3000, 4000, 4263.0f, 5415.0f, 123.0f, 0.691f},
     };
+    // average slope
+    static constexpr dtypes::float32 Fmavg = (0.693f + 0.693f + 0.702f + 0.691f) / 4.0f;
 
     // convert rm to step
     dtypes::uint16 rpmToStep(dtypes::uint16 _rpm, bool _checkLimits = true)
     {
         // deal with limits
-        if (_checkLimits && _rpm <= FminRpm)
+        if (_checkLimits && _rpm <= minSpeed)
             return FminStep;
-        if (_checkLimits && _rpm >= FmaxRpm)
+        if (_checkLimits && _rpm >= maxSpeed)
             return FmaxStep;
         // find calibration
         dtypes::float32 rpm = static_cast<dtypes::float32>(_rpm);
@@ -82,9 +81,9 @@ private:
     {
         // deal with limits
         if (_step <= FminStep)
-            return FminRpm;
+            return minSpeed;
         if (_step >= FmaxStep)
-            return FmaxRpm;
+            return maxSpeed;
         // find calibration
         dtypes::uint8 calib_idx = 0;
         for (; calib_idx < 4; calib_idx++)
@@ -97,57 +96,74 @@ private:
     }
 
 public:
-    sdds_var(Tuint16, steps, sdds::opt::nothing, 0);
+    sdds_var(Tuint16, minSpeed, sdds::opt::readonly, 50);
+    sdds_var(Tuint16, maxSpeed, sdds::opt::readonly, 5000);
     sdds_var(Tuint16, targetSpeed, sdds::opt::nothing, 0);
-    sdds_var(Tuint16, measuredSpeed, sdds::opt::nothing, 0);
+    sdds_var(Tuint16, measuredSpeed, sdds::opt::readonly, 0);
+    sdds_var(Tuint16, targetSteps, sdds::opt::nothing, 0);
+    sdds_var(Tuint16, steps, sdds::opt::readonly, 0);
+    using TonOff = sdds::enums::OnOff;
+    sdds_var(TonOff, autoAdjust, sdds::opt::nothing, TonOff::ON);
+    // speed tolerance: based on the calibration slopes of ~0.7 steps/rpm --> 1/0.7 = 1.4 rpm/step --> aim for targetSpeed +/- 2)
+    sdds_var(Tuint8, autoAdjustSpeedTolerance, sdds::opt::nothing, static_cast<dtypes::uint8>(ceil(1.0f / Fmavg)));
     sdds_var(Tuint16, speedCheckInterval, sdds::opt::nothing, 1000);
+    sdds_var(Tuint32, speedCheckCounter, sdds::opt::nothing, 0);
     sdds_enum(none, noResponse) Terror;
     sdds_var(Terror, error, sdds::opt::readonly);
 
     ThardwareMotor()
     {
 
-        FminStep = rpmToStep(FminRpm, false);
-        FmaxStep = rpmToStep(FmaxRpm, false);
+        FminStep = rpmToStep(minSpeed, false);
+        FmaxStep = rpmToStep(maxSpeed, false);
 
+        // set actual motor steps
         on(steps)
         {
-            if (steps == 0)
+            if (Finitalized)
+            {
+                if (steps == 0)
+                {
+                    // we can't tell if there is an error if we're not running
+                    error = Terror::none;
+                }
+                analogWrite(FspeedPin, steps.Fvalue, FspeedFreq);
+                FspeedStart = millis();
+                speedCheckCounter = 0;
+            }
+        };
+
+        // set target steps
+        on(targetSteps)
+        {
+            if (targetSteps == 0)
             {
                 // turn motor off
-                if (Finitalized)
-                {
-                    analogWrite(FspeedPin, 0, FspeedFreq);
-                    FspeedStart = millis();
-                }
+                steps = 0;
                 if (targetSpeed != 0)
                 {
-                    // check to aovid trigger loop
+                    // check to avoid trigger loop
                     targetSpeed = 0;
                 }
             }
-            else if (steps < FminStep)
+            else if (targetSteps < FminStep)
             {
                 // jump to the min step
-                steps = FminStep;
+                targetSteps = FminStep;
             }
-            else if (steps > FmaxStep)
+            else if (targetSteps > FmaxStep)
             {
                 // back down to max step
-                steps = FmaxStep;
+                targetSteps = FmaxStep;
             }
             else
             {
                 // set steps
-                if (Finitalized)
-                {
-                    analogWrite(FspeedPin, steps.Fvalue, FspeedFreq);
-                    FspeedStart = millis();
-                }
-                if (targetSpeed != stepToRpm(steps.Fvalue))
+                steps = targetSteps;
+                if (targetSpeed != stepToRpm(targetSteps.Fvalue))
                 {
                     // check to aovid trigger loop
-                    targetSpeed = stepToRpm(steps.Fvalue);
+                    targetSpeed = stepToRpm(targetSteps.Fvalue);
                 }
             }
         };
@@ -155,13 +171,26 @@ public:
         // set speed
         on(targetSpeed)
         {
-            if (targetSpeed == 0 && steps != 0)
+            if (targetSpeed == 0)
             {
-                steps = 0;
+                // loop safety check
+                if (targetSteps != 0)
+                    targetSteps = 0;
             }
-            else if (steps != rpmToStep(targetSpeed.Fvalue))
+            else if (targetSpeed < minSpeed)
             {
-                steps = rpmToStep(targetSpeed.Fvalue);
+                // jump to min speed
+                targetSpeed = minSpeed;
+            }
+            else if (targetSpeed > maxSpeed)
+            {
+                // jump to max speed
+                targetSpeed = maxSpeed;
+            }
+            else if (targetSteps != rpmToStep(targetSpeed.Fvalue))
+            {
+                // set target steps
+                targetSteps = rpmToStep(targetSpeed.Fvalue);
             }
         };
 
@@ -177,17 +206,35 @@ public:
             // decoder info: rpm = freq (in Hz) / 100 * 60 = counts / dt.ms * 1000 * 1/100 * 60 = count / dt.ms * 600.
             float rpm = (600. * FdecoderCounter) / (millis() - FdecoderStart);
             measuredSpeed = static_cast<dtypes::uint16>(round(rpm));
-            if (steps > 0 && FdecoderCounter < 10 && (millis() - FspeedStart) > 2 * speedCheckInterval)
+            speedCheckCounter++;
+            // are we up and running? (i.e. already at the 2nd speed check and set to something larger than 0)
+            if (speedCheckCounter > 1 && targetSteps > 0)
             {
-                // even at 50pm, the FdecoderCounter should be counting at least 80 cts/sec after the second speed check interval
-                if (error != Terror::noResponse)
-                    // mark as motor not having a response
-                    error = Terror::noResponse;
-            }
-            else if (error != Terror::none)
-            {
-                // all good
-                error = Terror::none;
+                // are we having a connectivity issue?
+                if (FdecoderCounter < 10)
+                {
+                    // even at 50pm, the FdecoderCounter should be counting at least 80 cts/sec by the 2nd speed check
+                    // (1st check can be slow to adjust)
+                    if (error != Terror::noResponse)
+                        // mark as motor not having a response
+                        error = Terror::noResponse;
+                }
+                else
+                {
+                    // all good, we're running for real
+                    if (error != Terror::none)
+                        // mark motor as not having an error
+                        error = Terror::none;
+                    // now that we're running, do we want to auto-adjust the steps to get closer to the targetSpeed?
+                    if (autoAdjust == TonOff::ON &&
+                        abs(measuredSpeed - targetSpeed) > autoAdjustSpeedTolerance)
+                    {
+                        // finetune step adjustments
+                        dtypes::uint8 adjustment = static_cast<dtypes::uint8>(round(static_cast<dtypes::float32>(abs(measuredSpeed - targetSpeed)) * Fmavg));
+                        if (adjustment > 0)
+                            steps = steps + adjustment * ((measuredSpeed < targetSpeed) ? 1 : -1);
+                    }
+                }
             }
             resetDecoder();
         };
