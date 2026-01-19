d43707cbad7107ef4ebbc44a83ca72b9b9316786
Sebastian Kopf <seb.kopf@gmail.com>
2026-01-19 11:02:12 -0700

    first draft of display ssd display class

---
+++ src/uDisplay.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "uTypedef.h"
+#include "Particle.h"
+#include "uHardwareOledSSD1309.h"
+#include "splash.h"
+
+class Tdisplay : public ThardwareOledSSD1309
+{
+
+public:
+    // enumerations
+    using align = ThardwareOledSSD1309::align;
+    using valign = ThardwareOledSSD1309::valign;
+
+    // display positions
+    static const uint16_t dividerX = 63;
+    static const uint16_t dividerY = 14;
+    static const uint16_t offsetX = dividerX + 2; // where the second column starts
+    static const uint16_t iconsY = 0;
+    static const uint16_t headerY = 3;
+    static const uint16_t line1Y = 17;
+    static const uint16_t line2Y = line1Y + 8;
+    static const uint16_t line3Y = line2Y + 8;
+    static const uint16_t line4Y = line3Y + 8;
+    static const uint16_t line5Y = line4Y + 8;
+    static const uint16_t line6Y = line5Y + 8;
+
+    // overall screen structure features
+    void drawLayoutLines()
+    {
+        drawLine(0, dividerY, width(), dividerY, 1);
+        drawLine(dividerX, dividerY, dividerX, height(), 1);
+    }
+
+    // indicators icons (always in the upper right corner)
+    // default height is 13 pixels
+    void drawIcon(const uint8_t *_icon, uint16_t _w, uint16_t _h = 13)
+    {
+        // FIXME: implement keeping track of the indicator icons in order here!
+        // need a reset function as well
+        drawBitmap(Tdisplay::iconsY, _icon, _w, _h, Tdisplay::align::RIGHT, FiconXpadding);
+        FiconXpadding += _w + 1;
+    }
+
+    // clear display
+    void clearDisplay()
+    {
+        Adafruit_SSD1306::clearDisplay();
+        FiconXpadding = 0;
+    }
+
+private:
+    // icon x padding
+    uint8_t FiconXpadding = 0;
+
+    // render splash screen content
+    virtual void splash() override
+    {
+        printBitmap(height() / 2, splash1_data, splash1_width, splash1_height, align::CENTER, valign::CENTER);
+    }
+
+public:
+    // display refresh rate
+    sdds_var(Tuint32, refresh_MS, sdds::opt::nothing, 1000);
+
+    // initialize 128x64 oled display with width, height, reset pin, and lower clock speed
+    // (screen does not get messed up as often at 100kHz instead of the 400kHz default)
+    Tdisplay() : ThardwareOledSSD1309(128, 64, D10, 100000)
+    {
+    }
+};
--- src/uHardwareOledSSD1309.h
+++ src/uHardwareOledSSD1309.h
@@ -7,22 +7,28 @@
 #include "uHardwareI2C.h"
 #define SSD1306_NO_SPLASH
 #include "Adafruit_SSD1306.h"
-#include "splash.h"
 
-class ThardwareOledSSD1309 : public ThardwareI2C
+class ThardwareOledSSD1309 : public ThardwareI2C, public Adafruit_SSD1306
 {
 
 public:
     // enumerations
     using TonOff = sdds::enums::OnOff;
+    sdds_enum(___, complete) Tstartup;
+    enum class align
+    {
+        LEFT,
+        CENTER,
+        RIGHT
+    };
+    enum class valign
+    {
+        TOP,
+        CENTER,
+        BOTTOM
+    };
 
 protected:
-    // // display object
-    Adafruit_SSD1306 display;
-    dtypes::uint16 Fwidth;
-    dtypes::uint16 Fheight;
-
-private:
     Ttimer FstartupTimer;
 
     // I2C write function
@@ -31,26 +37,26 @@ private:
         if (status != Tstatus::connected && !connect())
             return false;
 
-        // clear
-        display.clearDisplay();
-
-        // fill
-        if (splashscreen == TonOff::ON)
-            splash();
-        else
-            screen();
-
         // send over I2C
         WITH_LOCK(Wire)
         {
-            display.display();
+            // make sure vertical offset stays at 0 (tends to get messed up after a while)
+            ssd1306_command1(SSD1306_SETSTARTLINE | 0x0);
+            // display actual text
+            display();
         }
         return true;
     }
 
-    // read from I2C device (not applicable)
-    virtual bool read() override
+    // reset  display
+    virtual bool reset() override
     {
+        // reset screen
+        WITH_LOCK(Wire)
+        {
+            if (!begin(SSD1306_SWITCHCAPVCC, Fi2cAddress, true, false))
+                return false;
+        }
         return true;
     }
 
@@ -68,22 +74,18 @@ public:
     // splashscreen
     sdds_var(TonOff, splashscreen, sdds::opt::nothing, TonOff::ON);
     sdds_var(Tuint32, startup_MS, sdds::opt::nothing, 6000);
+    sdds_var(Tstartup, startup, sdds::opt::readonly);
 
     // constructor
-    ThardwareOledSSD1309(const uint16_t _width, const uint16_t _height) : Fwidth(_width), Fheight(_height), display(_width, _height)
+    ThardwareOledSSD1309(
+        uint8_t _width, const uint8_t _height, int8_t _rstPin,
+        uint32_t _clkDuring = 400000, uint32_t _clkAfter = 100000) : Adafruit_SSD1306(_width, _height, &Wire, _rstPin, _clkDuring, _clkAfter)
     {
-
         // startup timer
         on(FstartupTimer)
         {
-            if (splashscreen == TonOff::ON)
-                splashscreen = TonOff::OFF;
-        };
-
-        // splashscreen switch
-        on(splashscreen)
-        {
             action = ThardwareI2C::Taction::write;
+            startup = Tstartup::complete;
         };
     }
 
@@ -95,13 +97,14 @@ public:
             WITH_LOCK(Wire)
             {
                 ThardwareI2C::init(_i2cAddress);
-                display.begin(SSD1306_SWITCHCAPVCC, Fi2cAddress);
+                // begin with reset = true and periphBegin = false (Wire is already started separately in the I2C base class)
+                begin(SSD1306_SWITCHCAPVCC, Fi2cAddress, true, false);
             }
 
-            display.setTextWrap(0);      // don't wrap text by default
-            display.setTextSize(1);      // smallest text size by default
-            display.setTextColor(WHITE); // default color
-            display.clearDisplay();
+            setTextWrap(0);      // don't wrap text by default
+            setTextSize(1);      // smallest text size by default
+            setTextColor(WHITE); // default color
+            clearDisplay();
             if (splashscreen == TonOff::ON)
             {
                 splash();
@@ -109,8 +112,79 @@ public:
             }
             WITH_LOCK(Wire)
             {
-                display.display();
+                display();
             }
+            // for the next print
+            clearDisplay();
         }
+    }
+
+    // print bitmap
+    void printBitmap(uint16_t _y, const uint8_t *_bitmap, uint16_t _w, uint16_t _h, uint16_t _xpad = 0, valign _valign = valign::TOP)
+    {
+        drawBitmap(_y, _bitmap, _w, _h, align::LEFT, _xpad, _valign);
+    }
+
+    void printBitmap(uint16_t _y, const uint8_t *_bitmap, uint16_t _w, uint16_t _h, align _align, valign _valign)
+    {
+        drawBitmap(_y, _bitmap, _w, _h, _align, 0, _valign);
+    }
+
+    void drawBitmap(uint16_t _y, const uint8_t *_bitmap, uint16_t _width, uint16_t _height, align _align, uint16_t _xpad = 0, valign _valign = valign::TOP)
+    {
+        uint16_t x = _xpad;
+        if (_align == align::CENTER)
+        {
+            x = (width() - _width) / 2;
+        }
+        else if (_align == align::RIGHT)
+        {
+            x = width() - _width - _xpad;
+        }
+        uint16_t y = _y;
+        if (_valign == valign::CENTER)
+        {
+            y = (_y < _height / 2) ? 0 : _y - _height / 2;
+        }
+        else if (_valign == valign::BOTTOM)
+        {
+            y = (_y < _height) ? 0 : _y - _height;
+        }
+        Adafruit_SSD1306::drawBitmap(x, y, _bitmap, _width, _height, 1);
+    }
+
+    // print text line
+    void printLine(uint16_t _y, dtypes::string _line, uint16_t _xpad = 0, valign _valign = valign::TOP)
+    {
+        printLine(_y, _line, align::LEFT, _xpad, _valign);
+    }
+
+    void printLine(uint16_t _y, dtypes::string _line, align _align, valign _valign)
+    {
+        printLine(_y, _line, _align, 0, _valign);
+    }
+
+    void printLine(uint16_t _y, dtypes::string _line, align _align, uint16_t _xpad = 0, valign _valign = valign::TOP)
+    {
+        uint16_t x = _xpad;
+        if (_align == align::CENTER)
+        {
+            x = (width() - _line.length() * 6) / 2;
+        }
+        else if (_align == align::RIGHT)
+        {
+            x = width() - _line.length() * 6 - _xpad;
+        }
+        uint16_t y = _y;
+        if (_valign == valign::CENTER)
+        {
+            y = (_y < 4) ? 0 : _y - 4;
+        }
+        else if (_valign == valign::BOTTOM)
+        {
+            y = (_y < 8) ? 0 : _y - 8;
+        }
+        setCursor(x, y);
+        print(_line);
     }
 };
