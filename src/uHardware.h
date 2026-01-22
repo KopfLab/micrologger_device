166b124ad880fb509b557d79d45604d09fe9030f
Sebastian Kopf <seb.kopf@gmail.com>
2026-01-22 14:02:04 -0700

    update hardware to include dimmer

--- src/uHardware.h
+++ src/uHardware.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "uTypedef.h"
+#include "enums.h"
 #include "uMultask.h"
 #ifdef SDDS_ON_PARTICLE
 #include "uParticleSystem.h"
@@ -11,6 +12,12 @@
 #include "uHardwareIOExpanderTCA9534.h"
 #include "uHardwareRheostatMCP4652.h"
 #include "uHardwarePwmPCA9633.h"
+#include "uDisplay.h"
+
+// hardware constants
+#define MICROLOGGER_SIGNAL_PIN A1
+#define MICROLOGGER_SPEED_PIN A2
+#define MICROLOGGER_DECODER_PIN D6
 
 /**
  * @brief hardware handler
@@ -22,14 +29,19 @@ class Thardware : public TmenuHandle
 
 public:
     // enumerations
-    using TonOff = sdds::enums::OnOff;
-    using Ti2cStatus = ThardwareIOExpander::Tstatus;
     using Ti2cAction = ThardwareI2C::Taction;
+    using Ti2cError = ThardwareI2C::Terror;
+
     using TioMode = ThardwareIOExpander::Tmode;
     using TioValue = ThardwareIOExpander::Tvalue;
-    using TdimmerValue = ThardwarePwmPCA9633::Tvalue;
+
+    using TlightValue = ThardwarePwmPCA9633::Tvalue;
+    using TfanValue = ThardwarePwmPCA9633::Tvalue;
+
+    using TmotorError = ThardwareMotor::Terror;
 
 private:
+    // is the hardware initialized?
     bool Finitialized = false;
 
     // in case it is used in a tree
@@ -42,24 +54,96 @@ private:
     {
         if (!Finitialized)
         {
-            motor.init(A2, D6); // on particle, might be different on other devices!
+            display.init(particleSystem().version.value());
             expander.init();
-            dpot.init(ThardwareRheostatMCP4652::Resolution::S256, ThardwareRheostatMCP4652::Resistance::R100k);
+            //  dpot.init(ThardwareRheostatMCP4652::Resolution::S256, ThardwareRheostatMCP4652::Resistance::R100k);
             dimmer.init(ThardwarePwmPCA9633::Driver::EXTN);
+            motor.init(MICROLOGGER_SPEED_PIN, MICROLOGGER_DECODER_PIN);
+            Finitialized = true;
         }
     }
 
 public:
     // hardware components
-    sdds_var(Ti2cStatus, device, sdds::opt::readonly, Ti2cStatus::disconnected);
-    sdds_var(TonOff, beam, sdds::opt::nothing, TonOff::e::OFF);
-    sdds_var(TonOff, lights, sdds::opt::nothing, TonOff::e::OFF);
-    sdds_var(Tuint8, intensity, sdds::opt::nothing, 0);
-    sdds_var(TonOff, fan, sdds::opt::nothing, TonOff::e::OFF);
-    sdds_var(ThardwareMotor, motor);
+    sdds_var(Tdisplay, display);
     sdds_var(ThardwareIOExpander, expander);
     sdds_var(ThardwareRheostatMCP4652, dpot);
     sdds_var(ThardwarePwmPCA9633, dimmer);
+    sdds_var(ThardwareMotor, motor);
+
+    // aliases
+    decltype(expander.value1) &i2cValue = expander.value1;
+
+    decltype(expander.action) &beamAction = expander.action;
+    decltype(expander.pin2) &beamState = expander.pin2;
+    decltype(expander.value2) &beamValue = expander.value2;
+
+    decltype(dimmer.action) &fanLightAction = dimmer.action;
+    decltype(dimmer.error) &fanLightError = dimmer.error;
+    decltype(dimmer.status) &fanLightStatus = dimmer.status;
+
+    decltype(dimmer.state1) &lightState = dimmer.state1;
+    decltype(dimmer.value1) &lightValue = dimmer.value1;
+    decltype(dimmer.setpoint1) &lightSetpoint = dimmer.setpoint1;
+
+    decltype(dimmer.state2) &fanState = dimmer.state2;
+    decltype(dimmer.value2) &fanValue = dimmer.value2;
+    decltype(dimmer.setpoint2) &fanSetpoint = dimmer.setpoint2;
+
+    // set light by percentage
+    void setLight(enums::ToffOn::e _state, uint8_t _percent = 100)
+    {
+
+        if ((_state == enums::ToffOn::off || _percent == 0) && lightValue != TlightValue::OFF)
+        {
+            // full off
+            lightState = enums::ToffOn::off;
+            fanLightAction = Ti2cAction::write;
+        }
+        else if (_state == enums::ToffOn::on && _percent == 100 && lightValue != TlightValue::ON)
+        {
+            // full on
+            lightState = enums::ToffOn::on;
+            lightSetpoint = ThardwarePwmPCA9633::MAX;
+            fanLightAction = Ti2cAction::write;
+        }
+        else if (_state == enums::ToffOn::on && _percent < 100)
+        {
+            // dimmed
+            dtypes::uint8 setpoint = static_cast<dtypes::uint8>(round(static_cast<dtypes::float32>(_percent) * ThardwarePwmPCA9633::MAX / 100.));
+            if (lightValue != TlightValue::DIMMED || lightSetpoint != setpoint)
+            {
+                lightState = enums::ToffOn::on;
+                lightSetpoint = setpoint;
+                fanLightAction = Ti2cAction::write;
+            }
+        }
+    }
+
+    // set fan
+    void setFan(enums::ToffOn::e _state)
+    {
+
+        if (_state == enums::ToffOn::off && fanValue != TfanValue::OFF)
+        {
+            // fan off
+            fanState = enums::ToffOn::off;
+            fanLightAction = Ti2cAction::write;
+        }
+        else if (_state == enums::ToffOn::on && fanValue != TfanValue::ON)
+        {
+            // full on
+            fanState = enums::ToffOn::on;
+            fanSetpoint = ThardwarePwmPCA9633::MAX;
+            fanLightAction = Ti2cAction::write;
+        }
+    }
+
+    // read signal
+    dtypes::int32 readSignal()
+    {
+        return analogRead(MICROLOGGER_SIGNAL_PIN);
+    }
 
     // constructor
     Thardware()
@@ -83,138 +167,29 @@ public:
 #endif
 
         // turn the gpio expander connection autocheck on to keep track of device connection
-        expander.autoConnect = TonOff::ON;
+        expander.autoConnect = enums::ToffOn::on;
         expander.pin1 = TioMode::OUTPUT_ON;  // indicator light
         expander.pin2 = TioMode::OUTPUT_OFF; // beam LED
 
-        // check expander I2C status
+        // deal with i2c connection from auto-connecting expander
         on(expander.status)
         {
-            // if connected and not on yet, turn on
-            if (expander.status == Ti2cStatus::connected && expander.value1 != TioValue::ON)
+            // are we connected?
+            if (expander.status == enums::TconStatus::connected)
             {
-                expander.pin1 = TioMode::OUTPUT_ON;
-                expander.action = Ti2cAction::write;
-            }
-        };
+                // turn on connection indicator LED
+                expander.pin1 = Thardware::TioMode::OUTPUT_ON;
+                expander.action = Thardware::Ti2cAction::write;
 
-        // keep I2C status updated
-        on(expander.value1)
-        {
-            if (expander.value1 == TioValue::ON && device == Ti2cStatus::disconnected)
-                device = Ti2cStatus::connected;
-            else if (expander.value1 != TioValue::ON && device == Ti2cStatus::connected)
-                device = Ti2cStatus::disconnected;
-        };
-
-        // turn beam on/off
-        on(beam)
-        {
-            if (beam == TonOff::OFF && expander.value2 == TioValue::ON)
-            {
-                expander.pin2 = TioMode::OUTPUT_OFF;
-                expander.action = Ti2cAction::write;
-            }
-            else if (beam == TonOff::ON && expander.value2 != TioValue::ON)
-            {
-                expander.pin2 = TioMode::OUTPUT_ON;
-                expander.action = Ti2cAction::write;
-            }
-        };
-
-        // keep beam status updated
-        on(expander.value2)
-        {
-            if (expander.value2 == TioValue::ON && beam == TonOff::OFF)
-                beam = TonOff::ON;
-            else if (expander.value2 != TioValue::ON && beam == TonOff::ON)
-                beam = TonOff::OFF;
-        };
-
-        // dimmer I2C connection
-        on(device)
-        {
-            if (device == Ti2cStatus::connected)
-            {
-                // make sure to write configuration whenever I2C connects
+                // make sure to write configuration for dimmer on connect
                 dimmer.action = Ti2cAction::write;
             }
-            else if (device == Ti2cStatus::disconnected && dimmer.status == Ti2cStatus::connected)
+            else
             {
                 // flag dimmer as disconnected when I2C disconnects
-                dimmer.action = Ti2cAction::disconnect;
+                if (dimmer.status == enums::TconStatus::connected)
+                    dimmer.action = Ti2cAction::disconnect;
             }
-        };
-
-        // turn lights on/off / dim lights
-        on(lights)
-        {
-            dtypes::uint8 setpoint = static_cast<dtypes::uint8>(round(static_cast<dtypes::float32>(intensity.value()) * ThardwarePwmPCA9633::MAX / 100.));
-            if ((lights == TonOff::OFF || intensity == 0) && dimmer.value1 != TdimmerValue::OFF)
-            {
-                dimmer.state1 = TonOff::OFF;
-                dimmer.action = Ti2cAction::write;
-            }
-            else if (lights == TonOff::ON && intensity == 100 && dimmer.value1 != TdimmerValue::ON)
-            {
-                dimmer.setpoint1 = ThardwarePwmPCA9633::MAX;
-                dimmer.state1 = TonOff::ON;
-                dimmer.action = Ti2cAction::write;
-            }
-            else if (lights == TonOff::ON && intensity < 100 && (dimmer.value1 != TdimmerValue::DIMMED || dimmer.setpoint1 != setpoint))
-            {
-                dimmer.setpoint1 = setpoint;
-                dimmer.state1 = TonOff::ON;
-                dimmer.action = Ti2cAction::write;
-            }
-        };
-
-        // keep lights status updated
-        on(dimmer.value1)
-        {
-            if ((dimmer.value1 == TdimmerValue::ON || dimmer.value1 == TdimmerValue::DIMMED) && lights == TonOff::OFF)
-                lights = TonOff::ON;
-            else if ((dimmer.value1 != TdimmerValue::ON && dimmer.value1 != TdimmerValue::DIMMED) && lights == TonOff::ON)
-                lights = TonOff::OFF;
-        };
-
-        // change light intensity
-        on(intensity)
-        {
-            if (intensity == 0)
-                intensity = 1;
-            else if (intensity > 100)
-                intensity = 100;
-            else
-                lights.signalEvents();
-        };
-        intensity = 100;
-
-        // turn fan on/off
-        // FIXME: double check this works with disconnects/reconnects
-        on(fan)
-        {
-            if (fan == TonOff::OFF && dimmer.value2 != TdimmerValue::OFF)
-            {
-                dimmer.state2 = TonOff::OFF;
-                dimmer.action = Ti2cAction::write;
-            }
-            else if (fan == TonOff::ON && dimmer.value2 != TdimmerValue::ON)
-            {
-                dimmer.setpoint2 = ThardwarePwmPCA9633::MAX;
-                dimmer.state2 = TonOff::ON;
-                dimmer.action = Ti2cAction::write;
-            }
-        };
-
-        // keep fan status updated
-        // FIXME: double check this works with disconnects/reconnects
-        on(dimmer.value2)
-        {
-            if (dimmer.value2 == TdimmerValue::ON && fan == TonOff::OFF)
-                fan = TonOff::ON;
-            else if (dimmer.value2 != TdimmerValue::ON && fan == TonOff::ON)
-                fan = TonOff::OFF;
         };
     }
 };
